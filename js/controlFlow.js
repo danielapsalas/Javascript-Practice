cf1c305208fd73019964eab16a7036f1049283f8
Daniela Salas <danielapiconsalas@gmail.com>
2023-05-02 17:10:02 -0500

    going over control flow statements - if, else, and else-if

---
+++ js/controlFlow.js
@@ -0,0 +1,25 @@
+//Going over basic control flow statements
+
+//if - The if statement is used to execute a block of code only if a certain condition is true. For example:
+let x = 5;
+if (x > 3) {
+    console.log("x is greater than 3");
+}
+
+//else - The else statement is used to execute a block of code if the condition of an if statement is false. For example:
+let y = 1;
+if (y > 3) {
+    console.log("x is greater than 3");
+} else {
+    console.log("x is less than or equal to 3");
+}
+
+//else if - The else if statement is used to add an additional condition to an if statement. For example:
+let z = 5;
+if (z < 3) {
+    console.log("x is less than 3");
+} else if (z < 6) {
+    console.log("x is between 3 and 5");
+} else {
+    console.log("x is greater than or equal to 6");
+}
