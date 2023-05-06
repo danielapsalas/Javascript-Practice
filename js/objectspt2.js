e6b3583da97faba9fc050f0f1c4b68d41b93ab1b
Daniela Salas <danielapiconsalas@gmail.com>
2023-05-06 13:14:49 -0500

    went over object literals, factories, constructors, constructor property, functions are objects, value vs reference types, adding or removing properties, and enumarating properties

--- js/objectspt2.js
+++ js/objectspt2.js
@@ -18,6 +18,8 @@
 
 //Factories - object literal syntax isn't ideal if it has behavior
 // behavior - if it has more than one method
+
+//factory function
 function createCircle(radius){
     return {
         radius,//properties - holds value
@@ -38,3 +40,44 @@ function Circle(radius) {
 }
 
 const another = new Circle(1);//the new operator makes an empty object - if you don't use new you'll get a global variable
+//three things happen when you use the new operator
+//1st : the new operator will create an empty object
+//2nd : then it will set `this` to point to that object
+//3rd : it returns the object from the function
+
+//Each object has a constructor property and that references the function that was used to create that object
+
+//functions and arrays are Objects
+
+
+
+//adding or removing properties
+// const circle4 = new Circle(10);
+// circle4.location = {x: 1};
+//
+// // circle['location'] = {x: 1};
+// //lets imagine we have a location property
+// const propertyName = 'location';//you can use the bracket notation if you have special characters
+// circle4[propertyName] = {x: 1};
+// //how to delete properties you don't want the client to see
+// delete circle['location'];
+
+
+
+//Enumerating properties
+const circle10 = new Circle(10);
+
+//
+for(let key in circle){
+    if(typeof circle10[key] !== 'function')
+        console.log(key, circle10[key])//you can use bracket notation to access a member
+}
+
+//how to get all they keys in an object - as an array
+const keys = Object.keys(circle);
+console.log(keys);
+
+//does the object have a given property - check for existence
+if('radius' in circle){
+    console.log("Circle has a radius.")
+}
